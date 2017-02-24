13ee8ab53e4fa2346de6e76dd1d6072619d37d6c
Leo <leo.lou@gov.bc.ca>
2017-02-24 14:12:40 -0800

    Update api.go

--- api/api.go
+++ api/api.go
@@ -10,9 +10,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/etsy/hound/config"
-	"github.com/etsy/hound/index"
-	"github.com/etsy/hound/searcher"
+	"hound/config"
+	"hound/index"
+	"hound/searcher"
 )
 
 const (
